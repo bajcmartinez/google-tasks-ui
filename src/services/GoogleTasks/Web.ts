0d6c9006425b29d6e754aec9e28ac1839ca425a9
Juan Cruz Martinez <ZOJCRUZM@zeiss.com>
2019-09-16 12:55:18 +0200

    fix(auth): Fix issue with new auth

--- src/services/GoogleTasks/Web.ts
+++ src/services/GoogleTasks/Web.ts
@@ -12,8 +12,6 @@ export class GoogleTasksWebService {
   private isLoaded: boolean = false;
   private auth: any;
 
-  constructor() {}
-
   /**
    * Loads the client library and gets all the api required information from google servers
    *
@@ -97,6 +95,7 @@ export class GoogleTasksWebService {
    */
     subscribeSigninStatus (subscriber: (status: boolean) => void) {
     if (!this.auth) return false;
+    subscriber(this.isSignedIn());
     return this.auth.isSignedIn.listen(subscriber);
   }
 
